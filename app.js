1651e7dab78d83a3feda5124315808eaae49a6b4
tanvirabsur <tanvirabsar@yahoo.com>
2025-12-06 18:14:26 +0600

    setup api

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { client } = require('./db');
+const { ObjectId } = require('mongodb');
 const router = express.Router();
 
 
-const collection = client.db('data-of-college').collection('data')
+const collection = client.db('data-of-college').collection('data');
+const usersCollection = client.db('users').collection('users');
 
 router.get('/', (req,res)=>{
     res.send('Hello from the router!');
@@ -14,13 +16,29 @@ router.get('/colleges', async (req,res)=>{
     res.send(colleges);
 })
 
-router.get('/colleges/:id', (req,res)=>{
-    const collegeId = req.params.id;
-    res.send(`Details of college with ID: ${collegeId}`);
+router.get('/users', async (req,res)=>{
+    const data = await usersCollection.find({}).toArray();
+    res.send(data);
 });
 
-router.get('/reviews', (req,res)=>{
-    res.send('List of reviews will be here.');
+router.post('/create-user', async (req,res)=>{
+    const newUser = req.body;
+    const result = await usersCollection.insertOne(newUser);
+    res.send(result);
 });
+
+router.get('/colleges/:id', async (req,res)=>{
+    const collegeId = req.params.id;
+    const data = await collection.findOne({});
+    const college = data.colleges.find(c => c.id === collegeId);
+    res.send(college || { message: 'College not found' });
+});
+
+router.get('/reviews', async (req,res)=>{
+    const data = await collection.findOne({});
+    res.send(data.reviews);
+});
+
+
 
 module.exports = router
